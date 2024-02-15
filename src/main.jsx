9e74ff9c6eb318aea994e667a41d944ba802f903
rifaldiarifin <adirifaldiarifin@gmail.com>
2024-02-16 00:13:53 +0700

    edit main, implement dashboard ui, router, and CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './assets/webfonts/Montserrat.css'
+import './styles/globals.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/index.route'
+import DashboardUIContextProvider from './contexts/useDashboardUIContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <DashboardUIContextProvider>
+      <RouterProvider router={router} />
+    </DashboardUIContextProvider>
+  </React.StrictMode>
 )
