ff66a6f05719d8a6450210048e02e3b1687e26b3
rifaldiarifin <adirifaldiarifin@gmail.com>
2024-02-16 16:48:25 +0700

    fix bug: useEffect set to localstorage problem

--- src/hooks/useDemoChatbox.jsx
+++ src/hooks/useDemoChatbox.jsx
@@ -1,57 +1,59 @@
 import { useEffect, useRef, useState } from 'react'
 
 export default function useDemoChatbox() {
-  const ref = useRef(false)
+  const ref2 = useRef(false)
+  const [isLoad, setIsLoad] = useState(false)
   const [clientProfile, _setClientProfile] = useState({
     name: 'Esther Howard',
-    profileImg: '/common/avatar2.png'
+    profileImg: '/common/avatar2.webp'
   })
   const initChatMessage = [
     {
-      itsMe: true,
-      message: 'Are you ready?'
+      message: 'Are you ready?',
+      itsMe: true
     },
     {
-      itsMe: false,
-      message: 'I have prepared everything'
+      message: 'I have prepared everything',
+      itsMe: false
     }
   ]
-  const [chatMessages, _setChatMessages] = useState([])
+  const [chatMessages, _setChatMessages] = useState(initChatMessage)
 
   const getData = localStorage.getItem('DEMO_SIMPLE_CHATBOX')
 
   const setToLocal = (payload) => {
+    console.log(payload)
     localStorage.setItem('DEMO_SIMPLE_CHATBOX', JSON.stringify(payload))
   }
   const setClientProfile = ({ name, profileImg }) => {
     _setClientProfile({ name, profileImg })
   }
   const setChatMessages = (payload) => {
+    console.log('load inside', payload)
     _setChatMessages(payload)
   }
 
   const sendMessage = (message) => {
+    console.log(message)
     _setChatMessages([...chatMessages, { message, itsMe: true }])
   }
 
   // get local storage data
   useEffect(() => {
-    console.log('load outside', ref.current)
-    if (!ref.current) {
+    console.log('load outside', ref2.current)
+    if (!ref2.current) {
       getData ? setChatMessages(JSON.parse(getData)) : setToLocal(initChatMessage)
-
-      console.log('load inside', ref.current)
-      return () => (ref.current = true)
+      setIsLoad(true)
+      return () => (ref2.current = true)
     }
   }, [getData])
 
   useEffect(() => {
-    console.log('hahahha outside', ref.current)
-    if (ref.current) {
-      console.log('hahahha inside', ref.current)
+    console.log('chat')
+    if (isLoad) {
       setToLocal(chatMessages)
     }
-  }, [chatMessages, ref])
+  }, [chatMessages])
 
-  return { clientProfile, chatMessages, sendMessage, setClientProfile }
+  return { clientProfile, chatMessages, sendMessage, setClientProfile, setChatMessages }
 }
